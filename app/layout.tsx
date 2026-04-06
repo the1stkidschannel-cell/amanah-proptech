caa79446b481e03d68cf44d482c9506d53a7289c
the1stkidschannel-cell <the1stkidschannel@gmail.com>
2026-04-06 20:04:00 +0200

    Fix: move localized global components inside LanguageProvider to fix prerender error

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,11 +46,11 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-[#022c22] text-white">
-        <Analytics />
-        <LegalDisclaimer />
-        <HelpdeskWidget />
         <AuthProvider>
           <LanguageProvider>
+            <Analytics />
+            <LegalDisclaimer />
+            <HelpdeskWidget />
             <DashboardShell>{children}</DashboardShell>
           </LanguageProvider>
         </AuthProvider>
